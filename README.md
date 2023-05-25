ea6e6e95e0133334d93a8db1f33e755b89a9e4bd
Kawogo <dkawogo79@gmail.com>
2023-05-25 11:31:19 +0300

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## UI Resources Management System
+
+System to manage UI resources
+
+#### License
+
+MIT
---
+++ resources_management/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "UI Resources Management System",
+			"type": "module",
+			"label": _("UI Resources Management System")
+		}
+	]
