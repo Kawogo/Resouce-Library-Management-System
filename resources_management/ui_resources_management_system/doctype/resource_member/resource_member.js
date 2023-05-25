bb2c5966c35ad61b5acb43de26c045666c2a78b8
Kawogo <dkawogo79@gmail.com>
2023-05-25 11:55:13 +0300

    First upload

---
+++ resources_management/ui_resources_management_system/doctype/resource_member/resource_member.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, David Mathew Kawogo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Resource Member', {
+	// refresh: function(frm) {
+
+	// }
+});
